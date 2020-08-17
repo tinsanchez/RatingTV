7dac93abf78fb16a3268b949326b127c64d4b5d4
Valentin Sanchez <elzero_2@hotmail.com>
2020-08-17 13:42:30 +0200

    Initial Commit

---
+++ RatingTV/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RatingTV
+//
+//  Created by Valentín Sánchez Campañón on 17/08/2020.
+//  Copyright © 2020 Valentín Sánchez Campañón. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
